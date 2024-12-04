4f8006bbd744c3359563240e8feaadba05f44d7d
Lucy <lucy@nix2twink.gay>
2024-12-04 16:51:35 +0100

    added emojis

--- src/features/install_package.ts
+++ src/features/install_package.ts
@@ -1,6 +1,6 @@
 import { runCommand } from '../command';
 import { consola } from 'consola';
-
+import ora from 'ora';
 export async function handleInstall(packageName: string, options: { quiet?: boolean; force?: boolean; verbose?: boolean; dryRun?: boolean; }) {
     try {
         let command = `brew install ${packageName}`;
@@ -16,11 +16,24 @@ export async function handleInstall(packageName: string, options: { quiet?: bool
         if (options.quiet) {
             command += ' --quiet';
         }
-        const result = await runCommand(command);
-        if (!options.quiet) {
-            consola.info(`✅ Successfully installed "${packageName}":\n${result}`);
+        const confirmation = await consola.prompt(`Do you want to install the following packages ${packageName}`, {
+            type: "confirm",
+        });
+
+        if (confirmation) {
+            const spinner = ora(`🔄 Installing ${packageName}...`).start(); // Start the spinner after confirmation
+
+            const result = await runCommand(command);
+            spinner.succeed(`✅ Successfully installed "${packageName}":\n${result}`);
+            if (!options.quiet) {
+                consola.info(`✅ Successfully installed "${packageName}":\n${result}`);
+            }
+        } else {
+            consola.warn('❌ Installation cancelled.');
+            process.exit(1);
         }
+
     } catch (error: any) {
-        consola.error(`Error installing "${packageName}": ${error.message}`);
+        consola.error(`❌ Error installing "${packageName}": ${error.message}`);
     }
 }
